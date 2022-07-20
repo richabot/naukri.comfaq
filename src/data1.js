82ee7f0a1e27b0eb0d9b4ad2737a893518d68630
richa shah <lavaformama@gmail.com>
2022-07-20 09:49:06 +0530

    richa

---
+++ src/data1.js
@@ -0,0 +1,52 @@
+const list2 = [
+  {
+    id: 1,
+    title: "Mexico",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 2,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 3,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 4,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 5,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 6,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+  {
+    id: 7,
+    title: "Italy",
+   
+    img: "https://cdn.shopify.com/s/files/1/0906/2558/products/01.jpg?v=1644399711",
+    
+  },
+]
+export default list2;
